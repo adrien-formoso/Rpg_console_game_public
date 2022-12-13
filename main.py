1467cb929f3227e096f256d1bdc373c39bb99f7d
aformoso <adrien.formoso.af@gmail.com>
2022-12-13 22:20:24 +0100

    mise a jour systeme auto gen map

--- main.py
+++ main.py
@@ -33,24 +33,31 @@ class arche:
         "potion" : 0,
     }
 
-def generation_de_map(map):
-  for i in range(100):
-    map.append("-")
-  return map
-
 def creation_liste_position_dispo():
   liste = []
   for i in range(0,100):
     liste.append(i)
   return liste
 
-def generateur_villages(map,nb_villages):
+def generation_de_map():
+  map = []
+  for i in range(100):
+    map.append("-")
+  
+  return map
+
+def generateur_villages():
   global premier_villages_position
   global deuxieme_villages_position
+  global nb_villages
+
+
+  map = generation_de_map()
 
   liste_position_dispo = creation_liste_position_dispo()
+  
 
-  for i in range(1,nb_villages+1):
+  for i in range(1,4):
     position = liste_position_dispo[random.randint(0,99)]
     
     del liste_position_dispo[position]
@@ -58,44 +65,33 @@ def generateur_villages(map,nb_villages):
     map[position] = i
     if i == 1:
       premier_villages_position = position
-    else : 
+    elif i == 2 : 
       deuxieme_villages_position = position
+    else :
+      troisieme_villages_position = position
   return map
 
 
-def afficher_map(map):
+
+
+def afficher_map():
+  global nb_villages
+  global premier_villages_position
+  global deuxieme_villages_position
   i = 0
+  map = generateur_villages()
+  
   while i < len(map):
     if i%10 == 0:
       print()
     print(map[i],end = " ")
     i += 1
-  print()
-
-def connexion_villages(premiere_position,deuxieme_position,map):
-  #verifie si les villages sont sur la meme collonnes 
-  dernier_chifre_premiere_position = str(premiere_position)[-1]
-  dernier_chifre_deuxieme_position = str(deuxieme_position)[-1]
-  premier_chifre_premiere_position = str(premiere_position)[0]
-  premier_chifre_deuxieme_position = str(premiere_position)[0]
-
-  if dernier_chifre_premiere_position == dernier_chifre_deuxieme_position:
-    return
-  return
-    
 
 joueur_stats = mage()
 
-carte = []
-premier_villages_position = 0
-deuxieme_villages_position = 0
+nb_villages = 3
+premier_villages_position   = 0
+deuxieme_villages_position  = 0
+troisieme_villages_position = 0
 
-
-generation_de_map(carte)
-
-nb_villages = 2
-
-generateur_villages(carte,nb_villages)
-print(f"il y a {nb_villages} villages")
-print(f"la position du premier est en {premier_villages_position} et le deuxieme {deuxieme_villages_position}")
-afficher_map(carte)
+afficher_map()
